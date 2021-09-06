2c02f2200b033f467bc28d30fbbbd77fa89862be
czg <1210245540@qq.com>
2021-09-06 17:51:40 +0800

    20210906_10

---
+++ src/string/String387.java
@@ -0,0 +1,32 @@
+package string;
+
+import java.util.Arrays;
+
+/**
+ * @Author czgggggggg
+ * @Date 2021/9/6
+ * @Description
+ */
+public class String387 {
+    public static void main(String[] args) {
+        System.out.println(firstUniqChar("leetcode"));
+        System.out.println(firstUniqChar("loveleetcode"));
+        System.out.println(firstUniqChar("aabb"));
+        System.out.println(firstUniqChar("aabbccd"));
+    }
+    public static int firstUniqChar(String s) {
+        int[] count_arr = new int[26];
+        Arrays.fill(count_arr,0);
+
+        for(int i = 0; i < s.length(); i++){
+            count_arr[s.charAt(i) - 97]++;
+        }
+
+        for(int i = 0; i < s.length(); i++){
+            if(count_arr[s.charAt(i) - 97] == 1)
+                return i;
+        }
+
+        return -1;
+    }
+}
