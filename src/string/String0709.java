74951f894635c241794541a6c889e184211b8cbc
czg <1210245540@qq.com>
2021-09-07 15:18:22 +0800

    20210907_8

---
+++ src/string/String0709.java
@@ -0,0 +1,17 @@
+package string;
+
+/**
+ * @Author czgggggggg
+ * @Date 2021/9/7
+ * @Description
+ */
+public class String0709 {
+    public static void main(String[] args) {
+        System.out.println(toLowerCase("Hello"));
+        System.out.println(toLowerCase("here"));
+        System.out.println(toLowerCase("LOVELY"));
+    }
+    public static String toLowerCase(String s) {
+        return s.toLowerCase();
+    }
+}
