dbaab17a53ba42433d8a5af0d305faefec550507
czg <1210245540@qq.com>
2021-09-16 15:40:03 +0800

    20210916_5

---
+++ src/math/Math258.java
@@ -0,0 +1,27 @@
+package math;
+
+/**
+ * @Author czgggggggg
+ * @Date 2021/9/16
+ * @Description
+ */
+public class Math258 {
+    public static void main(String[] args) {
+        System.out.println(addDigits(38));
+        System.out.println(addDigits(111));
+        System.out.println(addDigits(1));
+    }
+    public static int addDigits(int num) {
+        int tmp;
+        while(num > 9){
+            tmp = 0;
+            while(num != 0){
+                tmp += (num % 10);
+                num /= 10;
+            }
+            num = tmp;
+        }
+
+        return num;
+    }
+}
