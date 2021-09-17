2bf7aeed2a0245640920be8fa344841ed51f7e6e
czg <1210245540@qq.com>
2021-09-17 16:32:41 +0800

    20210917_2

--- src/math/Math0326.java
+++ src/math/Math0326.java
@@ -1,33 +1,33 @@
-//package math;
-//
-///**
-// * @Author czgggggggg
-// * @Date 2021/9/17
-// * @Description
-// */
-//public class Math0326 {
-//    public static void main(String[] args) {
-//        System.out.println(isPowerOfThree(0));
-//        System.out.println(isPowerOfThree(1));
-//        System.out.println(isPowerOfThree(2));
-//        System.out.println(isPowerOfThree(3));
-//        System.out.println(isPowerOfThree(4));
-//        System.out.println(isPowerOfThree(5));
-//        System.out.println(isPowerOfThree(6));
-//        System.out.println(isPowerOfThree(7));
-//
-//        System.out.println(isPowerOfThree(27));
-//        System.out.println(isPowerOfThree(9));
-//        System.out.println(isPowerOfThree(45));
-//    }
-//    public static boolean isPowerOfThree(int n) {
-//        if(n == 0 || n == 2)
-//            return false;
-//        while (n != 1){
-//            if(n % 3 != 0)
-//                return false;
-//            n /= 3;
-//        }
-//        return true;
-//    }
-//}
+package math;
+
+/**
+ * @Author czgggggggg
+ * @Date 2021/9/17
+ * @Description
+ */
+public class Math0326 {
+    public static void main(String[] args) {
+        System.out.println(isPowerOfThree(0));
+        System.out.println(isPowerOfThree(1));
+        System.out.println(isPowerOfThree(2));
+        System.out.println(isPowerOfThree(3));
+        System.out.println(isPowerOfThree(4));
+        System.out.println(isPowerOfThree(5));
+        System.out.println(isPowerOfThree(6));
+        System.out.println(isPowerOfThree(7));
+
+        System.out.println(isPowerOfThree(27));
+        System.out.println(isPowerOfThree(9));
+        System.out.println(isPowerOfThree(45));
+    }
+    public static boolean isPowerOfThree(int n) {
+        if(n == 0 || n == 2)
+            return false;
+        while (n != 1){
+            if(n % 3 != 0)
+                return false;
+            n /= 3;
+        }
+        return true;
+    }
+}
