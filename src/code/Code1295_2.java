3264f10559b4769593701464a9ef96c368ac464b
czg <1210245540@qq.com>
2022-01-16 22:19:18 +0800

    20220116_01

---
+++ src/code/Code1295_2.java
@@ -0,0 +1,23 @@
+package code;
+
+/**
+ * @Author czgggggggg
+ * @Date 2022/1/16
+ * @Description
+ */
+public class Code1295_2 {
+    public static void main(String[] args) {
+        int[] nums1 = {12,345,2,6,7896};
+        System.out.println(findNumbers(nums1));
+        int[] nums2 = {555,901,482,1771};
+        System.out.println(findNumbers(nums2));
+    }
+    public static int findNumbers(int[] nums) {
+        int count = 0;
+        for(int num : nums){
+            if((num >= 10 && num <= 99) || (num >= 1000 && num <= 9999) || num == 100000)
+                count++;
+        }
+        return count;
+    }
+}
