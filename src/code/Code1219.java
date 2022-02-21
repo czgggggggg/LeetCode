3d4dd1d59e4c271f06ab55721fd4bed0444e27b2
czg <1210245540@qq.com>
2022-02-21 20:16:34 +0800

    20220221_09

---
+++ src/code/Code1219.java
@@ -0,0 +1,33 @@
+//package code;
+//
+///**
+// * @Author czgggggggg
+// * @Date 2022/2/21
+// * @Description
+// */
+//public class Code1219 {
+//    public static void main(String[] args) {
+//
+//    }
+//
+//    public static int getMaximumGold(int[][] grid) {
+//        int res = 0;
+//        int sum;
+//        int m = grid.length;
+//        int n = grid[0].length;
+//
+//        for(int i = 0; i < m; i++){
+//            for(int j = 0; j < n; j++){
+//                if(grid[i][j] != 0){//以不为0的单元出发
+//                    sum = 0;
+//
+//                }
+//            }
+//        }
+//
+//        return res;
+//    }
+//    public static void dfs(){
+//
+//    }
+//}
---
+++ src/code/Code1415.java
@@ -0,0 +1,61 @@
+package code;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * @Author czgggggggg
+ * @Date 2022/2/21
+ * @Description
+ */
+public class Code1415 {
+    public static void main(String[] args) {
+        System.out.println(getHappyString(1,3));//c
+        System.out.println(getHappyString(1,4));//
+        System.out.println(getHappyString(3,9));//cab
+        System.out.println(getHappyString(2,7));//
+        System.out.println(getHappyString(10,100));//abacbabacb
+    }
+
+    public static String getHappyString(int n, int k) {
+        List<String> res = new ArrayList<>();
+        StringBuilder sb = new StringBuilder();
+        int size = 0;
+        char ch;
+
+        for(int i = 97; i <= 99; i++){
+            ch = (char) i;
+            size++;
+            dfs(res,sb,size,ch,n,k);
+            size--;
+        }
+
+        if(k > res.size())
+            return "";
+        return res.get(k - 1);
+    }
+
+    public static void dfs(List<String> res, StringBuilder sb, int size, char ch, int n, int k){
+        sb.append(ch);
+
+        if(size == n){
+            res.add(sb.toString());
+            sb.deleteCharAt(sb.length() - 1);
+            return;
+        }
+
+        char tmp = ch;
+        for(int i = 97; i <= 99; i++){
+            ch = (char) i;
+            if(ch != tmp){
+                size++;
+                dfs(res,sb,size,ch,n,k);
+                size--;
+            }
+        }
+
+        sb.deleteCharAt(sb.length() - 1);
+    }
+}
