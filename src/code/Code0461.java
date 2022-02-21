34bee48fd937a340e4a256ee6a0773d289281b10
czg <1210245540@qq.com>
2022-02-21 20:55:29 +0800

    20220221_10

---
+++ src/code/Code0461.java
@@ -0,0 +1,31 @@
+package code;
+
+/**
+ * @Author czgggggggg
+ * @Date 2022/2/21
+ * @Description
+ */
+public class Code0461 {
+    public static void main(String[] args) {
+        System.out.println(hammingDistance(1,4));//2
+        System.out.println(hammingDistance(3,1));//1
+        System.out.println(hammingDistance(0,0));//0
+        System.out.println(hammingDistance(15,0));//4
+        System.out.println(hammingDistance(16,0));//1
+    }
+    public static int hammingDistance(int x, int y) {
+        int res = 0;
+        int i, j;
+
+        while(x != 0 || y != 0){
+            i = x % 2;
+            x /= 2;
+            j = y % 2;
+            y /= 2;
+            if(i != j)
+                res++;
+        }
+
+        return res;
+    }
+}
