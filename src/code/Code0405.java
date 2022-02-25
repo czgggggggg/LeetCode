fde12a6ee7e8575d9d2b217c16b7ef4a55f6b750
czg <1210245540@qq.com>
2022-02-25 23:43:37 +0800

    20220225_01

--- src/code/Code0405.java
+++ src/code/Code0405.java
@@ -6,4 +6,22 @@ package code;
  * @Description
  */
 public class Code0405 {
+    public static void main(String[] args) {
+        System.out.println(toHex(26));
+        System.out.println(toHex(-1));
+    }
+    public static String toHex(int num) {
+        if (num == 0) {
+            return "0";
+        }
+        StringBuffer sb = new StringBuffer();
+        for (int i = 7; i >= 0; i --) {
+            int val = (num >> (4 * i)) & 0xf;
+            if (sb.length() > 0 || val > 0) {
+                char digit = val < 10 ? (char) ('0' + val) : (char) ('a' + val - 10);
+                sb.append(digit);
+            }
+        }
+        return sb.toString();
+    }
 }
