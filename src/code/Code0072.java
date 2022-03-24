8c2ecc870cedaa5229d7fb5c30b371d1c7b716a8
czg <1210245540@qq.com>
2022-03-24 15:48:08 +0800

    20220324_04

---
+++ src/code/Code0072.java
@@ -0,0 +1,52 @@
+package code;
+
+/**
+ * @Author czgggggggg
+ * @Date 2022/3/24
+ * @Description
+ */
+public class Code0072 {
+    public static void main(String[] args) {
+        String word1_1 = "horse";
+        String word1_2 = "ros";
+        System.out.println(minDistance(word1_1,word1_2));//3
+
+        String word2_1 = "intention";
+        String word2_2 = "execution";
+        System.out.println(minDistance(word2_1,word2_2));//5
+    }
+    public static int minDistance(String word1, String word2) {
+        int n = word1.length();
+        int m = word2.length();
+
+        // 有一个字符串为空串
+        if (n * m == 0) {
+            return n + m;
+        }
+
+        // DP 数组
+        int[][] D = new int[n + 1][m + 1];
+
+        // 边界状态初始化
+        for (int i = 0; i < n + 1; i++) {
+            D[i][0] = i;
+        }
+        for (int j = 0; j < m + 1; j++) {
+            D[0][j] = j;
+        }
+
+        // 计算所有 DP 值
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j < m + 1; j++) {
+                int left = D[i - 1][j] + 1;
+                int down = D[i][j - 1] + 1;
+                int left_down = D[i - 1][j - 1];
+                if (word1.charAt(i - 1) != word2.charAt(j - 1)) {
+                    left_down += 1;
+                }
+                D[i][j] = Math.min(left, Math.min(down, left_down));
+            }
+        }
+        return D[n][m];
+    }
+}
