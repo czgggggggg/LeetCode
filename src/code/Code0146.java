78de16c7489a4aebda85f13d8377121a05cda73d
czg <1210245540@qq.com>
2022-03-31 20:53:35 +0800

    20220331_01

--- src/code/Code0146.java
+++ src/code/Code0146.java
@@ -19,17 +19,6 @@ public class Code0146 {
         System.out.println(lruCache.get(1));//-1
         System.out.println(lruCache.get(3));//3
         System.out.println(lruCache.get(4));//4
-
-//        LRUCache lRUCache = new LRUCache(2);
-//        lRUCache.put(1, 1); // 缓存是 {1=1}
-//        lRUCache.put(2, 2); // 缓存是 {1=1, 2=2}
-//        System.out.println(lRUCache.get(1));    // 返回 1
-//        lRUCache.put(3, 3); // 该操作会使得关键字 2 作废，缓存是 {1=1, 3=3}
-//        System.out.println(lRUCache.get(2));    // 返回 -1 (未找到)
-//        lRUCache.put(4, 4); // 该操作会使得关键字 1 作废，缓存是 {4=4, 3=3}
-//        System.out.println(lRUCache.get(1));   // 返回 -1 (未找到)
-//        System.out.println(lRUCache.get(3));   // 返回 3
-//        System.out.println(lRUCache.get(4));    // 返回 4
     }
 }
 class LRUCache{
